528795d4624e2711791089e8897d8a4a80e6ff97
Guillaume Lederrey <guillaume.lederrey@wikimedia.org>
2023-02-28 10:01:50 +0100

    cleanup of TestJavaEventSender

--- src/main/java/com/testjava/metrics_platform/TestJavaEventSender.java
+++ src/main/java/com/testjava/metrics_platform/TestJavaEventSender.java
@@ -1,9 +1,14 @@
 package com.testjava.metrics_platform;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URISyntaxException;
@@ -18,38 +23,40 @@ import org.wikimedia.metrics_platform.event.Event;
 import com.google.gson.Gson;
 
 public class TestJavaEventSender implements EventSender {
+
+    private final Gson gson = new Gson();
+
     @Override
-    public void sendEvents(String baseUri, Collection<Event> events) throws MalformedURLException {
+    public void sendEvents(String baseUri, Collection<Event> events) throws IOException {
         URL url = isValid(baseUri) ? new URL(baseUri) : new URL(DestinationEventService.LOCAL.getBaseUri());
 
+        HttpURLConnection connection = null;
         try {
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("POST");
             connection.setRequestProperty("Accept", "application/json");
             connection.setRequestProperty("Content-Type", "application/json");
             connection.setDoOutput(true);
+            connection.connect();
 
-            for (Event event : events) {
-                String payload = new Gson().toJson(event);
-                System.out.println(payload);
-                byte[] postData = payload.getBytes(StandardCharsets.UTF_8);
-                try (DataOutputStream dos = new DataOutputStream(connection.getOutputStream())) {
-                    dos.write(postData, 0, postData.length);
-                    System.out.println("Posted data to local event logging!");
+            try (Writer writer = new BufferedWriter(new OutputStreamWriter(connection.getOutputStream(), UTF_8))) {
+                for (Event event : events) {
+                    gson.toJson(event, writer);
                 }
-                try (BufferedReader br = new BufferedReader(
-                        new InputStreamReader(connection.getInputStream(), StandardCharsets.UTF_8))) {
-                    StringBuilder content = new StringBuilder();
-                    String inputLine;
-                    while ((inputLine = br.readLine()) != null) {
-                        content.append(inputLine.trim());
-                    }
-                    System.out.println(content);
-                }
+                System.out.println("Posted data to local event logging!");
             }
-            connection.disconnect();
-        } catch (IOException e) {
-            e.printStackTrace();
+
+            int status = connection.getResponseCode();
+            if (status == HttpURLConnection.HTTP_OK) {
+                try (BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), UTF_8))) {
+                    br.lines().forEach(System.out::print);
+                    System.out.println();
+                }
+            } else {
+                throw new IOException("Error sending events, status code=" + status);
+            }
+        } finally {
+            if (connection != null) connection.disconnect();
         }
     }
 
